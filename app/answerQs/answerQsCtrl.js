499fa168ba4ee3d4ab3078712af16cbc3f1d7c67
jorn55 <jorn55@gmail.com>
2019-03-09 21:47:45 +0200

    fixed jquery toggle

--- app/answerQs/answerQsCtrl.js
+++ app/answerQs/answerQsCtrl.js
@@ -1,5 +1,29 @@
+$(document).ready(function(){
+  $("#menu-toggle").click(function() {
+    $("#wrapper").toggleClass("toggled");
+  });
+});
+
+
+
 app.controller("answerQsCtrl", function($scope) {
 
-  $scope.items = ["Which country is the best?", "Which movie should I see this weekend?", "Which dress should I buy?"];
+  $scope.items = ["Which country is the best?", 
+  "Which movie should I see this weekend?", 
+  "Which dress should I buy?",
+  "Which shoes should I buy?",
+  "Which car is best?",
+  "Which song should I play at my wedding?", 
+  "Which movie should I see this weekend1?", 
+  "Which dress should I buy1?",
+  "Which shoes should I buy1?",
+  "Which car is best1?",
+  "Which song should I play at my wedding1?", 
+  "Which movie should I see this weekend2?", 
+  "Which dress should I buy2?",
+  "Which shoes should I buy2?",
+  "Which car is best2?",
+  "Which song should I play at my wedding2?"  
+];
   });
   
