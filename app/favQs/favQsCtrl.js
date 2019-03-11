dfc5d36def44aa5b8822744f1a4795ea4dbdbbcf
jorn55 <jorn55@gmail.com>
2019-03-11 16:35:15 +0200

    started working on favourites page

--- app/favQs/favQsCtrl.js
+++ app/favQs/favQsCtrl.js
@@ -1,4 +1,21 @@
-app.controller("favQsCtrl", function($scope) {
+app.controller("favQsCtrl", function($scope, questionSrv) {
+
+    var temp = questionSrv.getQuestions();
+
+    
+    $scope.items = temp;
+
+    $scope.quest = $scope.items[0];
+
+    $scope.cs1 = "fhvrd1";
+    $scope.cs2 = "fhvrd2";
+    $scope.cs3 = "fhvrd3";
+
+
+$scope.updateQuestion = function(item) {
+  $scope.quest = item;
+  // console.log(item);
+}
 
   
 });
